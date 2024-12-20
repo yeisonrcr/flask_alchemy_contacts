b72d5fa98cf6426a54638d2c8de9046137dae2d0
Yeison Araya <jeisonr0992@gmail.com>
2024-12-20 16:08:03 -0600

    actualizacion para deploy

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from routes.contacts import contacts
 from flask_sqlalchemy import SQLAlchemy
+
 from config import DATABASE_CONNECTION_URI
+from utils.db import db
+
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -27,3 +30,14 @@ SQLAlchemy(app)
 
 # Register the 'contacts' blueprint for modular route management
 app.register_blueprint(contacts)
+
+
+# Crear las tablas de la base de datos si no existen
+# Esto asegura que la estructura de la base de datos esté lista antes de ejecutar la aplicación
+with app.app_context(): #fuera el if name main
+    db.create_all()
+
+# Punto de entrada para ejecutar la aplicación Flask
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")  # Ejecuta la aplicación en modo debug, accesible desde cualquier IP
+
